6f21f2860997a6191e9516603e9cae590b788ce8
ITRabbitx <64525602+ITRabbitx@users.noreply.github.com>
2021-06-14 16:28:00 +0200

    Soda Machine Program
    Simple exercise program to exercise if / else if statements and getting a value from the user.

--- main.cpp
+++ main.cpp
@@ -4,8 +4,39 @@ using namespace std;
 
 int main()
 {
-    for (int x = 0; x<10; x++) {
-        cout << x << endl;
-    };
-return 0;
+
+    string cola = "cola";
+    string sprite = "sprite";
+    string water = "water";
+    string fanta = "fanta";
+    string icetea = "ice tea";
+
+\
+    int selectedNumber;
+
+
+    cout << "Select the beverage of your choice!" << endl;
+    cout << cola +",1 "+sprite+",2 "+water+",3 "+fanta+",4 "+icetea+",5 " << endl;
+    cin >> selectedNumber; // get the number from the user
+
+
+    //check what beverage was selected
+
+    if(selectedNumber == 1){
+        cout << "Your selected beverage is: "+cola << endl;
+    } else if(selectedNumber == 2){
+        cout << "Your selected beverage is: "+sprite << endl;
+    } else if (selectedNumber == 3){
+        cout << "Your selected beverage is: "+water << endl;
+    } else if (selectedNumber == 4){
+        cout << "Your selected beverage is: "+fanta << endl;
+    } else if (selectedNumber == 5){
+        cout << "Your selected beverage is: "+icetea << endl;
+    } else {
+        cout << "Error. Choice was not valid, here is your money back." << endl;
+    }
+
+
+
+    return 0;
 }
